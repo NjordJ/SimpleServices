082236dc1749d5798df60bb5b8a50b48ff72245b
NjordJ <nj0rd3841@gmail.com>
2022-10-22 21:13:16 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple services and work manager example
---
+++ app/src/main/java/com/iruda/servicesexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.iruda.servicesexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

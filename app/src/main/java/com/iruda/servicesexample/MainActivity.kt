5736f2403ac3395a558de5ba2d3733b209f7762e
NjordJ <nj0rd3841@gmail.com>
2022-10-23 12:34:34 +0500

    Revert "Launch notification"
    This reverts commit 0f6aff393a20ad24b68b4a8a5d80410b42a2b23c.

--- app/src/main/java/com/iruda/servicesexample/MainActivity.kt
+++ app/src/main/java/com/iruda/servicesexample/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.iruda.servicesexample
 
-import android.app.Notification
-import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.iruda.servicesexample.databinding.ActivityMainBinding
@@ -18,18 +16,5 @@ class MainActivity : AppCompatActivity() {
         binding.buttonSimpleService.setOnClickListener {
             startService(FirstService.newIntent(this, 25))
         }
-        binding.buttonForegroundService.setOnClickListener {
-            showNotification()
-        }
-    }
-
-    private fun showNotification() {
-        val notification = Notification.Builder(this)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .build()
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(1, notification)
     }
 }

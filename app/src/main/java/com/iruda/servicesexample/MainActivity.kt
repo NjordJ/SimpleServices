41db3e1a5ecd900c427ddece1ee9c70f7caa868b
NjordJ <nj0rd3841@gmail.com>
2022-10-23 12:46:05 +0500

    Launch local notification

--- app/src/main/java/com/iruda/servicesexample/MainActivity.kt
+++ app/src/main/java/com/iruda/servicesexample/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.iruda.servicesexample
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.iruda.servicesexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +21,33 @@ class MainActivity : AppCompatActivity() {
         binding.buttonSimpleService.setOnClickListener {
             startService(FirstService.newIntent(this, 25))
         }
+        binding.buttonForegroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                NOTIFICATION_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+
+        private const val NOTIFICATION_CHANNEL_ID = "notification_channel_id"
+        private const val NOTIFICATION_NAME = "notification_name"
+
     }
 }
